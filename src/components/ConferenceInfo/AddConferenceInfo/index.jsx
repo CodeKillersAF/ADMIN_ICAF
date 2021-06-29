b3932cbc0a90ce8fbf481468ee97b75dbd421289
kawsikan <kawsinote@gmail.com>
2021-06-29 10:19:01 +0530

    initial commit to add conference info

---
+++ src/components/ConferenceInfo/AddConferenceInfo/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class AddConferenceInfo extends React.Component {
+    
+    constructor(){
+        super(props);
+    }
+    
+    
+    
+    
+    
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
