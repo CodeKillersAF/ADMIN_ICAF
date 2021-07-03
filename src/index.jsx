db05aef1e2445b3a98a00c6a3a42f0b23dec08fc
TharushaDilmith <tharushadilmith99@gmail.com>
2021-07-03 18:30:24 +0530

    push changed protected routes

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import {render} from 'react-dom'
 import App from './App'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8080/api/';
+axios.defaults.baseURL = 'http://localhost:8080/api/users';
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
 
 render(<App/>,document.getElementById('app'));
